0f46de4e8738cca40f9035b90dbfdad5207a5a76
importTahsinZaman <tahsinz21366@gmail.com>
2023-03-03 23:29:49 -0500

    Meep Meep Auton Testing Setup

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -11,6 +11,7 @@ import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 public class MeepMeepTesting {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
+        RoadRunnerBotEntity myBot = null;
 
         boolean blue = true;
         boolean red = false;
@@ -18,15 +19,24 @@ public class MeepMeepTesting {
         boolean right = false;
 
         if (blue && right){
-            blueRight(meepMeep);
+            myBot = blueRight(meepMeep);
         }else if (red && left){
             redLeft(meepMeep);
         }else if (red && right){
             redRight(meepMeep);
         }else{
-            blueLeft(meepMeep);
+            myBot = blueLeft(meepMeep);
         }
 
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_KAI_DARK)
+//                .setTheme(new ColorSchemeBlueLight())
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+
+    private static RoadRunnerBotEntity blueLeft(MeepMeep meepMeep) {
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(52.48291908330528, 52.48291908330528, 2.9370625019, Math.toRadians(187.94061000000002), 12)
@@ -45,24 +55,72 @@ public class MeepMeepTesting {
 //                                .waitSeconds(.2) 120, 4
                                         .build()
                 );
-
-        meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_KAI_DARK)
-//                .setTheme(new ColorSchemeBlueLight())
-                .setBackgroundAlpha(0.95f)
-                .addEntity(myBot)
-                .start();
+        return myBot;
     }
 
-    private static void blueLeft(MeepMeep meepMeep) {
+    private static RoadRunnerBotEntity redRight(MeepMeep meepMeep) {
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(52.48291908330528, 52.48291908330528, 2.9370625019, Math.toRadians(187.94061000000002), 12)
+                .followTrajectorySequence(drive ->
+                                drive.trajectorySequenceBuilder(new Pose2d(36, -60, Math.toRadians(-180)))
+                                        .strafeRight(60)
+                                        .waitSeconds(.8)
+                                        .turn(Math.toRadians(140))
+//                                .splineTo(new Vector2d(58, -12), Math.toRadians(0))
+//                                .waitSeconds(.2)
+//                                .turn(Math.toRadians(180))
+//                                .splineTo(new Vector2d(36, 0), Math.toRadians(-180))
+//                                .splineTo(new Vector2d(36, 0), Math.toRadians(-180))
+//                                .waitSeconds(.2)
+//                                .splineTo(new Vector2d(58, -12), Math.toRadians(0))
+//                                .waitSeconds(.2) 120, 4
+                                        .build()
+                );
+        return myBot;
     }
 
-    private static void redRight(MeepMeep meepMeep) {
-
+    private static RoadRunnerBotEntity redLeft(MeepMeep meepMeep) {
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(52.48291908330528, 52.48291908330528, 2.9370625019, Math.toRadians(187.94061000000002), 12)
+                .followTrajectorySequence(drive ->
+                                drive.trajectorySequenceBuilder(new Pose2d(36, -60, Math.toRadians(-180)))
+                                        .strafeRight(60)
+                                        .waitSeconds(.8)
+                                        .turn(Math.toRadians(140))
+//                                .splineTo(new Vector2d(58, -12), Math.toRadians(0))
+//                                .waitSeconds(.2)
+//                                .turn(Math.toRadians(180))
+//                                .splineTo(new Vector2d(36, 0), Math.toRadians(-180))
+//                                .splineTo(new Vector2d(36, 0), Math.toRadians(-180))
+//                                .waitSeconds(.2)
+//                                .splineTo(new Vector2d(58, -12), Math.toRadians(0))
+//                                .waitSeconds(.2) 120, 4
+                                        .build()
+                );
+        return myBot;
     }
 
-    private static void redLeft(MeepMeep meepMeep) {
-    }
-
-    private static void blueRight(MeepMeep meepMeep) {
+    private static RoadRunnerBotEntity blueRight(MeepMeep meepMeep) {
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(52.48291908330528, 52.48291908330528, 2.9370625019, Math.toRadians(187.94061000000002), 12)
+                .followTrajectorySequence(drive ->
+                                drive.trajectorySequenceBuilder(new Pose2d(36, -60, Math.toRadians(-180)))
+                                        .strafeRight(60)
+                                        .waitSeconds(.8)
+                                        .turn(Math.toRadians(140))
+//                                .splineTo(new Vector2d(58, -12), Math.toRadians(0))
+//                                .waitSeconds(.2)
+//                                .turn(Math.toRadians(180))
+//                                .splineTo(new Vector2d(36, 0), Math.toRadians(-180))
+//                                .splineTo(new Vector2d(36, 0), Math.toRadians(-180))
+//                                .waitSeconds(.2)
+//                                .splineTo(new Vector2d(58, -12), Math.toRadians(0))
+//                                .waitSeconds(.2) 120, 4
+                                        .build()
+                );
+        return myBot;
     }
 }
